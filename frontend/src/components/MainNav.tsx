cd2d167b48f7d98a6874cde89811d257a34d4c11
biooids <biooidsbiooid@gmail.com>
2024-10-22 11:53:05 +0200

    "Added MainNav component to Header.tsx"

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./ui/button";
+
+function MainNav() {
+  return (
+    <Button className="hover:text-orange-500 hover:bg-orange-100">
+      MainNav
+    </Button>
+  );
+}
+
+export default MainNav;
