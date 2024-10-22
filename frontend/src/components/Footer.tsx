f86c3748bf72c57a89ba16a3d324228c6acd9ece
biooids <biooidsbiooid@gmail.com>
2024-10-22 12:49:38 +0200

    "Added Footer component to Layout"

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <div className="bg-orange-500 py-10">
+      <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+        <span className="text-3xl font-bold tracking-tight">Nzanira</span>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
