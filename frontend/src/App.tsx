404645959e80dbbcb4dd9a073126233fc95c9b09
biooids <biooidsbiooid@gmail.com>
2024-10-22 12:42:11 +0200

    "Added HomePage component to App.tsx and Hero component to Layout.tsx"

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Layout> </Layout>} />
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <HomePage />{" "}
+          </Layout>
+        }
+      />
       <Route path="/user-profile" element={<span>user profile</span>} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,17 @@
+function HomePage() {
+  return (
+    <div className="flex flex-col gap-12">
+      <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <h1
+          className="
+          text-3xl font-bold tracking-tight text-orange-500"
+        >
+          Nzanira, get your take away
+        </h1>
+        <span>Food is just a click away</span>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
