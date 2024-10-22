5faea639927e83a7b0f85905b5f2095f9831f099
biooids <biooidsbiooid@gmail.com>
2024-10-22 10:57:03 +0200

    connected to mongodb atlas

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,14 @@
 import express from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to data base"))
+  .catch((error) => {
+    console.log("Error connecting to data base: " + error);
+  });
 
 const app = express();
 app.use(express.json());
